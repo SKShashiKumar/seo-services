ba4463687d78be25085bd7aed19de508c9f9c787
Chandan Ramesh <chandan.doodleblue@gmail.com>
2022-02-14 19:14:04 +0530

    Half Pathway

---
+++ src/components/BrandPower/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BrandPower = () => {
+  return (
+    <div className="BrandPower">
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-6"></div>
+                <div className="col-6"></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BrandPower
